baa55e3d145165c9714b130064827cffa538da45
stefanbpeterson <stefanbpeterson@gmail.com>
2022-01-01 14:08:57 -0700

    adding new smart contract properties and new unit tests for those properties

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,12 @@
 pragma solidity ^0.5.0;
 
 contract Token {
-    string public name = 'My Name';
+    string public name = 'Art Token';
+    string public symbol = 'ART';
+    uint256 public decimals = 18;
+    uint public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000 * (10 ** decimals);
+    }
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -3,13 +3,37 @@ require('chai').use(require('chai-as-promised')).should()
 
 contract('Token', (accounts) => {
     describe('deployment', () => {
-        it ('tracks the name', async () => {
+        const name = 'Art Token'
+        const symbol = 'ART'
+        const decimals = '18'
+        const totalSupply = '1000000000000000000000000'
+        let token;
+
+        beforeEach(async() => {
             // Fetches token from the blockchain
-            const token = await Token.new()
+            token = await Token.new()
+        })
+
+        it('tracks the name', async() => {
             // Read token name here
             const result = await token.name()
-            // Check the token name is 'My Name'
-            result.should.equal('My Name')
+            // Check the token name is the same as the name in the smart contract for the token
+            result.should.equal(name)
+        })
+
+        it('tracks the symbol', async() => {
+            const result = await token.symbol()
+            result.should.equal(symbol)
+        })
+
+        it('tracks the decimals', async() => {
+            const result = await token.decimals()
+            result.toString().should.equal(decimals)
+        })
+
+        it('tracks the total supply', async() => {
+            const result = await token.totalSupply()
+            result.toString().should.equal(totalSupply)
         })
     })
 })
